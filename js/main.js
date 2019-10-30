30c835ddeec276ec40cd7eb06c76a5fbdce1a17c
Diego Vasquez <mail@diegovasquez.de>
2019-10-30 11:20:27 +0100

    ya encontré el error en sw.js, pero todavía pone problema con el jquery ","

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-window.onload = () => {
-  'use strict';
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js');
-  }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+   navigator.serviceWorker.register('./sw.js').then( () => {
+    console.log('Service Worker Registered')
+   })
+ })
 }
